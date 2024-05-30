aef1242a6febf9709f8b7b1a999c756afb6b9edb
liangyi <6373858+liangyi9812@users.noreply.github.com>
2024-05-31 03:51:47 +0800

    update send

--- api/send.ts
+++ api/send.ts
@@ -2,14 +2,22 @@ import { VercelRequest, VercelResponse } from "@vercel/node"
 import { bot, beforeHandle } from "../src/core"
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
-  beforeHandle(req, res, () => {
+  beforeHandle(req, res, async () => {
     const CHAT_ID = process.env.CHAT_ID || ""
     if (!CHAT_ID) {
       throw new Error("admin chat_id can't be null")
     }
-    bot.telegram.sendMessage(
+    const msg = await bot.telegram.sendMessage(
       CHAT_ID,
       JSON.stringify(req.body, null, 2) || "未收到任何消息, 请检查入参!"
     )
+    const {
+      chat: { id },
+      message_id,
+    } = msg
+    res.status(200).json({
+      chat_id: id,
+      message_id: message_id,
+    })
   })
 }
